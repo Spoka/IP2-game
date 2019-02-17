4f99933a3cbfb5167a24773478b9e48e88d770ce
Spoka <thekikogy@gmail.com>
2019-02-17 16:20:49 +0000

    first commits
    gotta fix cube n stuff

---
+++ IP2 Game/Assets/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour {
+
+    public float rotationTime = 0.5f;
+    public float rotationAgle = 90f;
+
+    public void CubePieceRotation()
+    {
+        if(gameObject.tag == "CubePiece")
+        {
+            Quaternion target = Quaternion.Euler(90, 0, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationTime);
+        }
+    }
+
+}
