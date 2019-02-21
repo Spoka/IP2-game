ffaf14963af86de957405d4d75b914f485e1c6e5
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-02-21 00:38:37 +0000

    player movement
    Player movement and push mechanics(still working)

---
+++ IP2 Game/Assets/Player2Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour {
+
+    public float movementSpeed;
+    Vector3 movementVelocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        movementVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            movementVelocity.z = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementVelocity.z = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementVelocity.x = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementVelocity.x = -1.0f;
+        }
+        transform.Translate(movementVelocity.normalized * Time.deltaTime * movementSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            transform.Rotate(0.0f, 200 * Time.deltaTime, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.N))
+        {
+            transform.Rotate(0.0f, -200 * Time.deltaTime, 0.0f);
+        }
+    }
+}
---
+++ IP2 Game/Assets/Push.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+
+    public GameObject CubeObject;
+
+    public GameObject Cube2Object;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            CubeObject.SetActive(true);
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            CubeObject.SetActive(false);
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+                Cube2Object.SetActive(false);
+
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+                Cube2Object.SetActive(true);
+
+            if (Input.GetKeyUp(KeyCode.RightShift))
+                CubeObject.SetActive(true);
+
+            else if (Input.GetKeyDown(KeyCode.RightShift))
+                CubeObject.SetActive(false);
+            {
+                if (Input.GetKeyUp(KeyCode.RightShift))
+                    Cube2Object.SetActive(false);
+
+                else if (Input.GetKeyDown(KeyCode.RightShift))
+                    Cube2Object.SetActive(true);
+            }
+
+        }
+    }
+}
