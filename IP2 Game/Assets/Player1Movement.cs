95000b6f19b62b0772e6418864ab7b0c540d798a
Spoka <thekikogy@gmail.com>
2019-02-22 22:03:09 +0000

    Repository update refill
    this should now be the new latest updated repo

---
+++ IP2 Game/Assets/Player1Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    
+
+    public float movementSpeed;
+    Vector3 movementVelocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        movementVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementVelocity.z = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementVelocity.z = -1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementVelocity.x = 1.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementVelocity.x = -1.0f;
+        }
+        transform.Translate(movementVelocity.normalized * Time.deltaTime * movementSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0.0f, 200 * Time.deltaTime, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0.0f, -200 * Time.deltaTime, 0.0f);
+        }
+    }
+}
---
+++ IP2 Game/Assets/Push.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void EnableRagdoll()
+    {
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+    }
+    void DisableRagdoll()
+    {
+        rb.isKinematic = true;
+        rb.detectCollisions = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            EnableRagdoll();
+        }
+        else if(collision.gameObject.tag == "Player2" && (Input.GetKey(KeyCode.RightShift)))
+        {
+            EnableRagdoll();
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            DisableRagdoll();
+        }
+        else if (collision.gameObject.tag == "Player2")
+        {
+            DisableRagdoll();
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    }
+
