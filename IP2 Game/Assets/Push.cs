c678f0278f8bfe4454e621c429647f8f753dff40
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-02-22 14:02:31 +0000

    Updated push script with tags

--- IP2 Game/Assets/Push.cs
+++ IP2 Game/Assets/Push.cs
@@ -4,45 +4,52 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
+    public Rigidbody rb;
+    
 
-    public GameObject CubeObject;
-
-    public GameObject Cube2Object;
-    // Use this for initialization
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
+    void EnableRagdoll()
+    {
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+    }
+    void DisableRagdoll()
+    {
+        rb.isKinematic = true;
+        rb.detectCollisions = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            EnableRagdoll();
+        }
+        else if(collision.gameObject.tag == "Player2" && (Input.GetKey(KeyCode.RightShift)))
+        {
+            EnableRagdoll();
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            DisableRagdoll();
+        }
+        else if (collision.gameObject.tag == "Player2")
+        {
+            DisableRagdoll();
+        }
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            CubeObject.SetActive(true);
-
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-            CubeObject.SetActive(false);
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-                Cube2Object.SetActive(false);
-
-            else if (Input.GetKeyUp(KeyCode.LeftShift))
-                Cube2Object.SetActive(true);
-
-            if (Input.GetKeyUp(KeyCode.RightShift))
-                CubeObject.SetActive(true);
-
-            else if (Input.GetKeyDown(KeyCode.RightShift))
-                CubeObject.SetActive(false);
-            {
-                if (Input.GetKeyUp(KeyCode.RightShift))
-                    Cube2Object.SetActive(false);
-
-                else if (Input.GetKeyDown(KeyCode.RightShift))
-                    Cube2Object.SetActive(true);
-            }
-
-        }
     }
-}
+    }
+
