9322671b0bb66cc9e060a611554845775cad1bab
Spoka <thekikogy@gmail.com>
2019-02-28 22:45:48 +0000

    added rotation warning and 6th section

--- IP2 Game/Assets/Scripts/Rotator.cs
+++ IP2 Game/Assets/Scripts/Rotator.cs
@@ -6,41 +6,43 @@ using UnityEngine.UI;
 public class Rotator : MonoBehaviour {
 
     public PlayerScript playerScript;
+    public GameObject rotationWarning;
     public float rotationAngle = 90f;
     public float rotationStart = 0f;
-    public float autoRotationTimer = 3.0f;
+    public float autoRotationTimer;
     float rT = 0;
 
+    //When player1 stands on a section for 2 or more seconds, start rotation coroutine 
     private void OnTriggerStay (Collider other)
     {
-        if (other.tag == "Player" || other.tag == "Player2")
+        if (other.tag == "Player")
         {
             rT += Time.deltaTime;
             if (rT >= autoRotationTimer)
             {
-                print("we fratm");
+                rotationWarning.SetActive(true);
                 StartCoroutine(NiceRotation(rotationStart, rotationAngle, 1.0f));
                 rT = 0;
             }
         }
     }
 
+    //Child Stuff object to the section it's in
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Stuff")
         {
             if (playerScript.isParent == false)
             {
-                print("suca");
                 other.transform.SetParent(gameObject.transform);
             }           
         }
     }
 
-
+    //Reset rotation timer if player1 exits the section
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player" || other.tag == "Player2")
+        if (other.tag == "Player")
         {
             if (rT <= 1.5f)
             {
@@ -49,14 +51,18 @@ public class Rotator : MonoBehaviour {
         }
     }
 
-    public void CubePieceRotation()
+    /*public void CubePieceRotation()
     {
         StartCoroutine(NiceRotation(rotationStart, rotationAngle, 1.0f));
-    }
+    }*/
   
+
+    //Rotation coroutine --- Lerp the section, show rotation warning, call method that updates lerp values
     IEnumerator  NiceRotation(float start, float end, float maxTime)
     {
         float t = 0;
+        yield return new WaitForSeconds(1);
+
         while (t < maxTime)
         {
             t += Time.deltaTime*2;
@@ -73,9 +79,11 @@ public class Rotator : MonoBehaviour {
             }
             yield return null;
         }
+        rotationWarning.SetActive(false);
         NewRotValues();
     }
 
+    //Update lerp values
     public void NewRotValues()
     {
         if (rotationAngle != 0)
