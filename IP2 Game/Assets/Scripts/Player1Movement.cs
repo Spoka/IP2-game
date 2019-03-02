dc7c3e621e5fef7d3a59f9b26f9dc0e52bf5a37a
Mewhirter <SMEWHI200@caledonian.ac.uk>
2019-03-02 13:38:59 +0000

    TIMED MOVEMENT

--- IP2 Game/Assets/Scripts/Player1Movement.cs
+++ IP2 Game/Assets/Scripts/Player1Movement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Player1Movement : MonoBehaviour
 {
-    
+
+    bool canMove = true;
+
+    public float coolDown = 0.5f;
 
     public float movementSpeed;
     Vector3 movementVelocity;
@@ -18,21 +21,33 @@ public class Player1Movement : MonoBehaviour
     void Update()
     {
         movementVelocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && canMove)
         {
             movementVelocity.z = 1.0f;
+            canMove = false;
+
+            Invoke("CooledDown", coolDown);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && canMove)
         {
             movementVelocity.z = -1.0f;
+            canMove = false;
+
+            Invoke("CooledDown", coolDown);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && canMove)
         {
             movementVelocity.x = 1.0f;
+            canMove = false;
+
+            Invoke("CooledDown", coolDown);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && canMove)
         {
             movementVelocity.x = -1.0f;
+            canMove = false;
+
+            Invoke("CooledDown", coolDown);
         }
         transform.Translate(movementVelocity.normalized * Time.deltaTime * movementSpeed, Space.World);
 
@@ -44,5 +59,12 @@ public class Player1Movement : MonoBehaviour
         {
             transform.Rotate(0.0f, -200 * Time.deltaTime, 0.0f);
         } 
+    }
+
+    void CooledDown()
+    {
+
+        canMove = true;
+
     }
 }
