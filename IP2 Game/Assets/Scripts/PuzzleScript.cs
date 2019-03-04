a08f614d88da850b2b6c69a31fabf4e6b212bed4
Mewhirter <SMEWHI200@caledonian.ac.uk>
2019-03-04 15:05:45 +0000

    Puzzle mechanics

---
+++ IP2 Game/Assets/Scripts/PuzzleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleScript : MonoBehaviour {
+
+    public PlayerScript playerScript;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Stuff")
+        {
+            if (playerScript.isParent == true)
+            {
+                other.transform.SetParent(gameObject.transform);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
