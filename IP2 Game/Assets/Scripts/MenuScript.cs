bd9c3bbb0dc4cba891fd5fbb39bcdeb0e294dbc0
ebrown212 <45566901+ebrown212@users.noreply.github.com>
2019-03-09 18:07:25 +0000

    Menu and start of UI

---
+++ IP2 Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void LoadMainLevel()
+    {
+        Application.LoadLevel("RotationTestScene");
+    }
+    public void LoadOptions()
+    {
+        Application.LoadLevel("OptionsMenu");
+    }
+    public void LoadMainMenu()
+    {
+        Application.LoadLevel("MenuScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
