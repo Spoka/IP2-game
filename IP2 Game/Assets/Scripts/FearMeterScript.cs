15efe47b59396775656a3772690d058f42c8ba8a
StevenMewhirter <45992104+StevenMewhirter@users.noreply.github.com>
2019-03-13 14:07:12 +0000

    fear meter working

--- IP2 Game/Assets/Scripts/FearMeterScript.cs
+++ IP2 Game/Assets/Scripts/FearMeterScript.cs
@@ -7,10 +7,10 @@ public class FearMeterScript : MonoBehaviour {
 
     // Use this for initialization
     public LifeLoss other;
-
+    bool isOutside = false;
     public Slider fearBar;
     public float fear;
-    private const float timeLapse = 0.5f;
+    private const float timeLapse = 2.0f;
 
     void Start()
     {
@@ -19,27 +19,39 @@ public class FearMeterScript : MonoBehaviour {
 
     void Update()
     {
-        fearBar.value = fear;
+        
     if (fear == 0f)
         {
             other.LoseHealth();
         }
-
-        fear -= timeLapse * Time.deltaTime;
-
+        if (isOutside == true)
+        {
+            fearBar.value = fear;
+            fear -= timeLapse * Time.deltaTime;
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         
         if (other.gameObject.tag == "Player")
         {
-            //fear = fear - lT / Time.deltaTime;
-            fear -= timeLapse * Time.deltaTime;
+            isOutside = true;
+            print("fear working");
         }
 
         if(fear == 50)
         {
             print("fear to 50");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isOutside = false;
+            print("is working fine");
         }
     }
 }
